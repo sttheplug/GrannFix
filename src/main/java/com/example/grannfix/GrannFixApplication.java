ddc691cbe2e20a0a2c458618ba2b36afac89e406
sttheplug <129483377+sttheplug@users.noreply.github.com>
2026-02-13 16:12:38 +0100

    Init

---
+++ src/main/java/com/example/grannfix/GrannFixApplication.java
@@ -0,0 +1,13 @@
+package com.example.grannfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrannFixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrannFixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grannfix/GrannFixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grannfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrannFixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
